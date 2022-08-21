1b97c7bec043b4d93d7a6ba0737fbaa9b0322ffc
paudy10 <javadbayrami79@gmail.com>
2022-08-21 14:31:34 +0430

    edit and del file in services

--- src/services/navLinks.js
+++ src/services/navLinks.js
@@ -1,18 +1,19 @@
 const navLinks = [
     { id: 1, text: 'صفحه اصلی', link: '/', icon: 'fa fa-home ' },
-    {
-        id: 2,
-        text: 'ارتباط با ما',
-        link: '/contact',
-        icon: 'fa fa-phone '
-    },
+
     {
         id: 3,
         text: 'بلاگ',
         link: '/blog',
         icon: 'fa fa-book'
     },
-    { id: 4, text: 'ثبت نام / ورود', link: '/login', icon: 'fa fa-sign-in'}
+    {
+        id: 2,
+        text: 'ارتباط با ما',
+        link: '/contact',
+        icon: 'fa fa-phone '
+    },
+    { id: 4, text: 'ثبت نام / ورود', link: '/login', icon: 'fa fa-sign-in' }
 ];
 
 const getNavLinks = () => {
